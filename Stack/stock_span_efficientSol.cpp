e79e2091fe4ee5abc1bdb7751c9f1873ca92b1c7
Pradeep Negi <58898980+pradeep-negi@users.noreply.github.com>
2022-05-14 16:45:16 +0530

    Create stock_span_efficientSol.cpp

---
+++ Stack/stock_span_efficientSol.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> stockSpanner(vector<int> &stocks){
+  
+	int n=stocks.size();
+	vector<int>span(n);
+	stack<int>s;
+	int i=0;
+	span[i]=1;
+    s.push(i);
+    int prev_idx=0;
+	for(i=1;i<n;i++){
+	    
+        while(stocks[i]>=stocks[s.top()] && !s.empty())
+            s.pop();
+        if(s.empty())
+            span[i]=i-prev_idx+1;
+            
+        else{
+            span[i]=i-s.top();
+            prev_idx=s.top();
+            s.push(i);
+        }
+	    
+	}
+	
+	return span;
+}
+
+int main()
+{
+    vector<int>stocks={100,80,60,70,60,75,85};
+    int n=stocks.size();
+    vector<int>span=stockSpanner(stocks);
+    for(int i=0;i<n;i++){
+        cout<<span[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
