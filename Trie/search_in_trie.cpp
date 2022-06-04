97aaf1da72420d32f8e45005217d4cbd38272576
Pradeep Negi <58898980+pradeep-negi@users.noreply.github.com>
2022-06-04 19:08:31 +0530

    Create search_in_trie.cpp

---
+++ Trie/search_in_trie.cpp
@@ -0,0 +1,67 @@
+ #include<iostream>
+ #include<unordered_map>
+ using namespace std;
+
+class Node{
+    public:
+
+    char ch;
+    unordered_map<char, Node*> mp;
+    bool terminal;
+
+    Node(char ch){
+        this->ch=ch;
+        terminal=false;
+    }
+
+};
+
+class Trie{
+
+    public:
+    Node*root;
+
+    Trie(){
+        root=new Node('\0');
+    }
+    
+
+    //insertion
+    void insert(string str){
+        Node *ptr=root;
+        int size=str.length();
+        for(int i=0;i<size;i++){
+            char ch=str[i];
+            if(ptr->mp.find(ch)==ptr->mp.end()){   //the element is not present in the map 
+                Node* temp=new Node(ch);
+                ptr->mp[ch]= temp;
+            }
+            ptr=ptr->mp[ch];
+        }
+        ptr->terminal=true;
+    }
+
+    //searching
+    bool search(string str){
+        Node *ptr=root;
+        int size=str.length();
+        for(int i=0;i<size;i++){
+            char ch=str[i];
+            if(ptr->mp.find(ch)==ptr->mp.end())
+                return false;
+            ptr=ptr->mp[ch];
+        }
+        return ptr->terminal;
+    }
+
+};
+
+ int main(){
+     Trie t;
+     t.insert("apple");
+     t.insert("app");
+     t.insert("news");
+     t.insert("no");
+     cout<<t.search("news");
+     return 0;
+ }
